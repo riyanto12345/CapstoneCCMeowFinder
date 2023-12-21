5edaae3ad034bb8f2b3681e24df6b0c385c18826
riyanto12345 <102023623+riyanto12345@users.noreply.github.com>
2023-12-21 22:22:09 +0700

    Update registerController.js

--- CapstoneCCMeow/controllers/registerController.js
+++ CapstoneCCMeow/controllers/registerController.js
@@ -1,38 +1,37 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const gensalt = bcrypt.genSaltSync(saltRounds); 
+const salt = bcrypt.genSaltSync(saltRounds); 
 const db = require('../middleware/dbConnection');
 
-exports.registerUser = async (req,res) =>{
-    const {email,username,password} = req.body;
-    if(!email||!username||!password){
+exports.registerUser = async (req, res) => {
+    const { email, username, password } = req.body;
+    if (!email || !username || !password) {
         return res.status(400).json({
-          msg:"Please fill all the fields"
+            message: "Please complete all the required fields"
         });
     }
 
-    const queryEmail = "SELECT * FROM tb_user WHERE email=?";
-    db.query(queryEmail,[email],(err,result) =>{
-        if(err) throw err;
-        if(result.length> 0){
+    const checkEmailQuery = "SELECT * FROM tb_user WHERE email=?";
+    db.query(checkEmailQuery, [email], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
             return res.status(400).json({
-              msg:"Email already exists"});
+                message: "Email already in use. Please choose another one."
+            });
         }
-  
-        const query = "INSERT INTO tb_user(email,username,password) VALUES (?,?,?)";
-        db.query(query,[email,username,bcrypt.hashSync(password,saltRounds)],(err,result) =>{
-            if(err) throw err;
-            return res.status(201).json({msg:"User registered successfully"});
-        })
-    })  
-    
-  }
 
-  exports.getUser = async (req, res) => {
+        const insertQuery = "INSERT INTO tb_user(email, username, password) VALUES (?,?,?)";
+        db.query(insertQuery, [email, username, bcrypt.hashSync(password, saltRounds)], (err, result) => {
+            if (err) throw err;
+            return res.status(201).json({ message: "User successfully registered" });
+        });
+    });
+}
+
+exports.getUser = async (req, res) => {
     const query = "SELECT * FROM tb_user";
     db.query(query, (err, result) => {
-      if (err) throw err;
-      return res.status(200).json({ result });
+        if (err) throw err;
+        return res.status(200).json({ users: result });
     });
-  };
-  
+};
